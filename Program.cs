8ffd0da815df47405181cee6916215cd7b1a79ce
Vleerian <doomjaw@hotmail.com>
2022-05-20 20:29:47 -0400

    Handle new regions (#7)
    * Refactor to add command line arguments
    
    * Increment Verison Number
    
    * Update README.md
    
    Add section detailing CLI usage
    
    * Handle regions that do not have lastupdate set
    
    Co-authored-by: Vleerian R <vleerian@remordias>

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Net;
+using System.Net;
 using System.Net.Http.Json;
 using System.Diagnostics.CodeAnalysis;
 using System.ComponentModel;
@@ -181,7 +181,9 @@ class FattKATTCommand : AsyncCommand<FattKATTCommand.Settings>
             }
 
             RegionAPI Region = Helpers.BetterDeserialize<RegionAPI>(await req.Content.ReadAsStringAsync());
-            if (current_time - Region.LastUpdate < 7200)
+            if(Region.LastUpdate == null)
+                Logger.Warning($"{trigger} is a new region.");
+            else if (current_time - Region.LastUpdate < 7200)
                 Logger.Warning($"{trigger} has already updated.");
             else
                 Sorted_Triggers.Add((Region.LastUpdate, trigger));
